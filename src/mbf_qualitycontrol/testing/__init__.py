9a8bd3a94e507bfdc03ddcb9264dbd975c963a92
finkernagel <finkernagel@imt.uni-marburg.de>
2019-03-29 09:59:26 +0000

    fixed base image path in assert_image_equal

--- src/mbf_qualitycontrol/testing/__init__.py
+++ src/mbf_qualitycontrol/testing/__init__.py
@@ -44,30 +44,56 @@ def caller_name(skip=2):
     del parentframe
     return ".".join(name)
 
+def caller_file(skip=2):
+    """Get a name of a caller in the format module.class.method
+
+       `skip` specifies how many levels of stack to skip while getting caller
+       name. skip=1 means "who calls me", skip=2 "who calls my caller" etc.
+
+       An empty string is returned if skipped levels exceed stack height
+    """
+
+    def stack_(frame):
+        framelist = []
+        while frame:
+            framelist.append(frame)
+            frame = frame.f_back
+        return framelist
+
+    stack = stack_(sys._getframe(1))
+    start = 0 + skip
+    if len(stack) < start + 1:
+        return ""
+    parentframe = stack[start]
+
+    module = inspect.getmodule(parentframe)
+    # `modname` can be None when frame is executed directly in console
+    # TODO(techtonik): consider using __main__
+    # detect classnamarentframe.f_locals["self"].__class__.__name__)
+    return parentframe.f_code.co_filename
+    
+
 
 def assert_image_equal(generated_image_path, suffix="", tolerance=2):
     """assert that the generated image and the base_images/{test_module}/{cls}/{function_name}{suffix}.extension is identical"""
     generated_image_path = Path(generated_image_path).absolute()
     extension = generated_image_path.suffix
     caller = caller_name(1)
+    caller_fn = caller_file(1)
     parts = caller.split(".")
-    if len(parts) == 3:
+    if len(parts) >= 3:
         func = parts[-1]
         cls = parts[-2]
         module = parts[-3]
-        if cls.lower() == cls:  # not actually a class, a module instead
-            module = cls
-            cls = "_"
+        #if cls.lower() == cls:  # not actually a class, a module instead
+            #module = cls
+            #cls = "_"
     else:
         module = parts[-2]
         cls = "_"
         func = parts[-1]
     should_path = (
-        Path(__file__).parent
-        / ".."
-        / ".."
-        / ".."
-        / "tests"
+        Path(caller_fn).parent
         / "base_images"
         / module
         / cls
